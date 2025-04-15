eec0a294faa7a9ff7ac3c671b07c8653cc8b35d2
Kashish Agarwal <agarwal.kashish82018@gmail.com>
2025-04-15 22:39:43 +0530

    updation

--- src/Components/TodoCard.jsx
+++ src/Components/TodoCard.jsx
@@ -41,12 +41,12 @@ const TodoCard = ({todo,setTodos,openu,view,theme}) => {
   
   if(view=="list"){
     return (
-      <div className={`h-[300px] w-[300px] border border-black/10 rounded-lg p-3 ${theme=="light"?"bg-[#FFB4A2]":"bg-black text-white"  }`}>
+      <div className={`h-[300px] w-[300px] rounded-lg p-3 ${theme=="light"?"bg-[#FFB4A2]  border border-black/10":"bg-black text-white  border border-white"  }`}>
         <div className='flex justify-between items-center '>
            <h3 className='font-semibold'>{title}</h3>
          <button onClick={toggleCompleted}>  {completed?"♥️":<div className='w-5 h-5 border border-black rounded-sm'></div>}</button>
         </div>
-    <div className='w-[270px] mt-2 h-[200px] border border-black/10 rounded-lg p-2 bg-[#FFF5E4]'><p className='text-sm break-words'>{description}</p></div>
+    <div className='w-[270px] mt-2 h-[200px] border border-black/10 rounded-lg p-2 bg-[#FFF5E4]'><p className={`text-sm break-words ${theme=="light"?"text-black":"text-black "}`}>{description}</p></div>
     <button className='border border-black p-[4px] bg-[#B03052] text-white rounded-lg mt-[10px] ml-[160px] text-xs' onClick={openUpModal}>Update</button>
     <button onClick={delfromlocalStorage} className='border border-black p-[4px] bg-[#B03052] text-white rounded-lg text-xs ml-2'>Remove</button>
     </div>
